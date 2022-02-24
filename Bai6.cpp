073cb49e8511b51d464b2a7ae9dd214f32fea5a7
guenfon <92385828+guenfon@users.noreply.github.com>
2022-02-24 16:21:08 +0700

    first commit

---
+++ Bai6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string ten1, ten2, ten3;
+    cin >> ten1 >> ten2 >> ten3;
+    cout << "Hi " << ten3 << ", " << ten2 << " and " << ten1 << ".";
+    return 0;
+}
